1b789ffa63433132d249bb333c5849cb82e418a3
Boyang Zhu <boyangzhu@Boyangs-MacBook-Pro.local>
2014-02-05 21:47:52 -0500

    fucking initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WebHax</title>
+</head>
+<body>
+<?php require_once('server/db.php'); ?>
+
+<h1>Malicious Code in Websites: Fun with SQL Injection, Cross-Site Scripting (XSS), and Cross-Site Request Forgery (CSRF)</h1>
+<h4>Boyang Zhu and Teja Ravipati</h4>
+<h4>Current Version: February 5th, 2014</h4>
+
+<form name="reset_db_form" action="server/reset_db.php" method="post">
+<a>Reset Database <input type="submit"></a>
+
+
+</form>
+
+</body>
+
+</html>
