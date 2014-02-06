6dfb03c7609fce7e9af44fc3f2db28ae04908f07
Teja Ravipati <ravipati@umich.edu>
2014-02-06 01:20:48 -0500

    XSS

--- index.php
+++ index.php
@@ -11,11 +11,16 @@
 <h4>Current Version: February 5th, 2014</h4>
 
 <form name="reset_db_form" action="server/reset_db.php" method="post">
-<a>Reset Database <input type="submit"></a>
+    Reset Database <input type="submit"> <br>
+</form>
+<br>
 
-
+<form action="index.php" method="post">
+    <input type="text" name="val"><br>
+    <input type="submit">
 </form>
 
-</body>
+<?php if (isset($_POST["val"])) echo ($_POST["val"]) ?>
 
+</body>
 </html>
